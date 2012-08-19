dd90f39fee4eaea21566b29159d7fc1b939089b9
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-08-19 00:32:36 -0400

    Updated plugin with documentation and examples
    - Added installation information
    - Added docs on proper usage of `Environment::configure()`
    - Added docs on recommended practices regarding database switching
    - Added sample environment bootstrap files
    - Added Environment class
    - Updated Environment class to always consider `CAKE_ENV` first

--- Config/bootstrap/environments/production.php
+++ Config/bootstrap/environments/production.php
@@ -1,7 +1,4 @@
 <?php
-App::uses('Environment', 'Lib');
-
-// config
 
 Environment::configure('production',
 	array(
@@ -18,7 +15,7 @@ Environment::configure('production',
 
 		// Debug should be off in production
 		'debug'                   => 0,
-		
+
 		// Securty
 		'Security.level'          => 'medium',
 		'Security.salt'           => 'SALT',
@@ -60,64 +57,3 @@ Environment::configure('production',
 		}
 	}
 );
-
-Environment::configure('staging',
-	array(
-		'server' => array('staging.example.com')
-	),
-	array(
-		// Site specific items
-		'Settings.FULL_BASE_URL'  => 'http://staging.example.com',
-
-		'Email.username'          => 'email@example.com',
-		'Email.password'          => 'password',
-		'Email.test'              => 'email@example.com',
-		'Email.from'              => 'email@example.com',
-
-		'logQueries'              => true,
-
-		// App Specific functions
-		'debug'                   => 0,
-
-		// Securty
-		'Security.level'          => 'medium',
-		'Security.salt'           => 'SALT',
-		'Security.cipherSeed'     => 'CIPHERSEED',
-	),
-	function() {
-		date_default_timezone_set('UTC');
-
-		Cache::config('default', array('engine' => 'File'));
-		if (!defined('FULL_BASE_URL')) {
-			define('FULL_BASE_URL', Configure::read('Settings.FULL_BASE_URL'));
-		}
-	}
-);
-
-Environment::configure('development',
-	true, // Defaults to development
-	array(
-		'Settings.FULL_BASE_URL'  => 'http://example.dev',
-
-		'Email.username'          => 'email@example.com',
-		'Email.password'          => 'password',
-		'Email.test'              => 'email@example.com',
-		'Email.from'              => 'email@example.com',
-
-		'logQueries'              => true,
-
-		'debug'                   => 2,
-		'Cache.disable'           => true,
-		'Security.salt'           => 'SALT',
-		'Security.cipherSeed'     => 'CIPHERSEED',
-	),
-	function() {
-		if (!defined('FULL_BASE_URL')) {
-			define('FULL_BASE_URL', Configure::read('Settings.FULL_BASE_URL'));
-		}
-	}
-);
-
-// run
-
-Environment::start();
---
+++ Lib/Environment.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Singleton class to handle environment specific configurations.
+ *
+ * Auto-detect environment based on specific configured params and
+ * allow per environment configuration and environment emulation.
+ *
+ * Environment. Smart Environment Handling.
+ * Copyright 2008 Rafael Bandeira - rafaelbandeira3
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ */
+class Environment {
+
+	public $environments = array();
+
+	protected $_configMap = array(
+		'security' => 'Security.level'
+	);
+
+	protected $_paramMap = array(
+		'server' => 'SERVER_NAME'
+	);
+
+	static function &getInstance() {
+		static $instance = array();
+		if (!isset($instance[0])) {
+			$Environment = 'Environment';
+			if (config('app_environment')) {
+				$Environment = 'App' . $Environment;
+			}
+			$instance[0] = new $Environment();
+			Configure::write('Environment.initialized', true);
+		}
+		return $instance[0];
+	}
+
+	static function configure($name, $params, $config = null, $callable = null) {
+		$_this = Environment::getInstance();
+		$_this->environments[$name] = compact('name', 'params', 'config', 'callable');
+	}
+
+	static function start($environment = null) {
+		$_this =& Environment::getInstance();
+		$_this->setup($environment);
+	}
+
+	static function is($environment) {
+		$_this =& Environment::getInstance();
+		return ($_this->name === $environment);
+	}
+
+	public function __construct() {
+		if (Configure::read('Environment.initialized')) {
+			throw new Exception('Environment can only be initialized once');
+			return false;
+		}
+	}
+
+	public function setup($environment = null) {
+		if (Configure::read('Environment.setup')) {
+			return;
+		}
+
+		$current = ($environment === null) ? 'development' : $environment;
+		if (empty($environment)) {
+			foreach ($this->environments as $name => $config) {
+				if ($this->_envMatch($name) || $this->_match($name, $config['params'])) {
+					$current = $name;
+					break;
+				}
+			}
+		}
+
+		$config = array_merge(
+			$this->environments[$current]['config'],
+			array('Environment.name' => $current)
+		);
+		foreach ($config as $param => $value) {
+			if (isset($this->_configMap[$param])) {
+				$param = $this->_configMap[$param];
+			}
+			Configure::write($param, $value);
+		}
+
+		if (is_callable($this->environments[$current]['callable'])) {
+			$this->environments[$current]['callable']();
+		}
+
+		if (Configure::read('debug') > 0) {
+			App::uses('CakeLog', 'Log');
+			if (class_exists('CakeLog')) {
+				CakeLog::write(LOG_INFO, $current);
+				Configure::write('Environment.setup', true);
+			}
+		}
+	}
+
+	protected function _envMatch($environment) {
+		return (isset($_SERVER['CAKE_ENV']) && $_SERVER['CAKE_ENV'] == $environment);
+	}
+
+	protected function _match($environment, $params) {
+		if ($params === true) {
+			return true;
+		}
+
+		foreach ($params as $param => $value) {
+			if (isset($this->_paramMap[$param])) {
+				$param = $this->_paramMap[$param];
+			}
+
+			if (function_exists($param)) {
+				$match = call_user_func($param, $value);
+			} elseif (is_array($value)) {
+				$match = in_array(env($param), $value);
+			} else {
+				$match = (env($param) === $value);
+			}
+
+			if (!$match) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
